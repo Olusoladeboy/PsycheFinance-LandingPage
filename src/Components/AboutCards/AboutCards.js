4f41b6fabfb9f827571d695f4a56811c7bbd45b6
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-08 00:40:54 +0100

    added section for button

--- src/Components/AboutCards/AboutCards.js
+++ src/Components/AboutCards/AboutCards.js
@@ -1,17 +1,20 @@
 import '../../Styles/AboutCards.css'
-const AboutCards = ({img, stroke, header, about}) =>{
-    return(
+const AboutCards = ({ img, stroke, header, about, button }) => {
+    return (
         <>
-        <div className="card-container">
-            <div className="card-content">
-                <img src={img} alt={img} />
-                <h3>{header}</h3>
-                <p>{about}</p>
+            <div className="card-container">
+                <div className="card-content">
+                    <img src={img} alt={img} />
+                    <div className="coming-soon-btn">
+                        {button}
+                    </div>
+                    <h3>{header}</h3>
+                    <p>{about}</p>
+                </div>
+                <div className="stroke">
+                    {stroke}
+                </div>
             </div>
-            <div className="stroke">
-                {stroke}
-            </div>
-        </div>
         </>
     )
 }
