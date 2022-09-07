c59b3e889d179b8e4c6bbab05ea642d093ea40d3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-08 00:38:17 +0100

    replaced facebook icon with medium

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,16 +6,16 @@ const Footer = () =>{
         <div className="footer-container">
             <div className="">
                 <div>
-                    <img src="images/psycheLogo.svg" />
+                    <img src="images/psycheLogo.svg" alt='' />
                     <p>Psyche Finance, Inc. is a financial technology company and is <br/> not a bank. </p>
                 </div>
             </div>
             <div className='footer-content'>
                <p className='copyright'> &copy; 2022 Psyche. All rights reserved</p>
                <div>
-                   <a href='#'><img src='images/linkedin.svg' alt='linkedin' /></a>
-                   <a href='#'><img src='images/twitter.svg' alt='twitter' /></a>
-                   <a href='#'><img src='images/facebook.svg' alt='facebook' /></a>
+                   <a href='https://www.linkedin.com/company/psyche-finance'><img src='images/linkedin.svg' alt='linkedin' /></a>
+                   <a href='https://twitter.com/Psychefinance'> <img src='images/twitter.svg' alt='twitter' /></a>
+                   <a href='https://psychefinance.medium.com'><img src='images/medium.svg' alt='medium' width='40px' height='32px' /></a>
                </div>
             </div>
         </div>
